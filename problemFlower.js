a1b2caa2303901d59d8f4c56527baf864cd2010f
Snehap979 <snehap9779@gmail.com>
2023-02-02 15:32:44 -0800

    Update problemFlower.js

--- problemFlower.js
+++ problemFlower.js
@@ -1,4 +1,3 @@
-function flowerbed(flowerbed,n){
 let count=0
 let i=0
 let adjacent=0
@@ -6,7 +5,13 @@ if(n==0)
 {
   return true
 }
-if(flowerbed[0]==0 && flowerbed[1]==0)
+if(flowerbed[0]==0 && n==1 && flowerbed.length==1)
+return true
+let start=flowerbed[0]
+
+let cur=flowerbed[i]
+let next=flowerbed[i+1]
+if(start==0 && next===0)
 {
   count=count+1
   if(count==n)
@@ -15,19 +20,39 @@ if(flowerbed[0]==0 && flowerbed[1]==0)
   }
   i=i+1
 }
-let cur=flowerbed[i]
-for(i;i<flowerbed.length;i++)
+cur=flowerbed[i]
+next=flowerbed[i+1]
+for(i;i<=flowerbed.length-2;i++)
 {
+ 
   let next=flowerbed[i+1]
   adjacent=flowerbed[i+2]
-  if(cur!=undefined && next!=undefined && cur^next===0)
+  if(adjacent==undefined && i==flowerbed.length-2)
+ {
+   //check if the cur and next are the ^ of each
+   if(cur!=undefined && next!=undefined && cur^next===0)
+   {
+     count=count+1
+      if(count==n)
+      {
+        return true
+      }
+   }
+ }
+  if(cur!=undefined && next!=undefined && adjacent!=undefined && cur^next===0)
 {
-  count=count+1
- next=next^1
-  if(count==n && (next^cur^adjacent!=0))
+  
+   
+ if(adjacent!=1)
+ {
+     next=next^1
+     count=count+1
+ if(count==n && (next^cur^adjacent!=0))
   {
     return true
   }
+ }
+ 
    
   cur=next
  
@@ -41,8 +66,4 @@ if(count==n)
 return (true)
 }
 else return(false)
-
-
-}
-console.log(flowerbed([1,0,0,1,0],2))
 
