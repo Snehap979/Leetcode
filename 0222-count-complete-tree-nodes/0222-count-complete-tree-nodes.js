717e38c4d598287dada3adb279443546be9577ff
Snehap979 <snehap9779@gmail.com>
2023-09-11 02:00:48 +0530

    Time: 107 ms (18.58%), Space: 69.6 MB (80.85%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+     let count=0
+  if(root)
+      {
+          arr=[root]
+          let deque=[]
+          let count=0
+          while(arr.length!=0)
+              {
+                  let node=arr.pop()
+                  // console.log("node.val",node.val)
+                  count=count+1
+                  if(node.left)
+                      {
+                          deque.push(node.left)
+                           // count=count+1
+                      }
+                  if(node.right)
+                      {
+                          deque.push(node.right)
+                           // count=count+1
+                      }
+                  // console.log("deque",deque)
+                  if(arr.length==0)
+                      {
+                          arr=[...deque ]
+                          // count=count+deque.length
+                          deque=[]
+                          
+                      }
+                 
+              }
+           return count
+      }
+   return count
+};
